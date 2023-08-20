a6f8215572726bce9098823246c4578d20766703
madraceee <raajanitheesh@gmail.com>
2023-08-20 13:45:36 +0530

    Landing page bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import ContactMe from './sections/ContactMe';
 
 function App() {
 
-  const [initialLoadAnimation, setInitialLoadAnimation] = useState<boolean>(false);
+  const [initialLoadAnimation, setInitialLoadAnimation] = useState<boolean>(true);
 
   const onComplete = () => {
     setInitialLoadAnimation(false);
